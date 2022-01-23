66d38955aeb523c64d2b6cfd75268d604573f6f7
mnikn <mnikn@outlook.com>
2022-01-23 09:26:17 +0800

    feat: update data

--- platform/Constants.gd
+++ platform/Constants.gd
@@ -9,34 +9,6 @@ const SKILLS = {
 		"normal_damage": 2,
 		"critical_damage": 5
 	},
-	"fire_missile": {
-		"id": "fire_ball",
-		"type": "attack",
-		"name": "火弹术",
-		"weak_damage": 1,
-		"normal_damage": 2,
-		"critical_damage": 5
-	},
-	"fire_attack": {
-		"id": "fire_attack",
-		"type": "buffer",
-		"name": "炙热能量",
-		"attack": 2,
-	},
-	"big_fire_attack": {
-		"id": "big_fire_attack",
-		"type": "buffer",
-		"name": "灼热力量",
-		"attack": 5
-	},
-	"fire_whip": {
-		"id": "fire_whip",
-		"type": "attack",
-		"name": "火鞭",
-		"weak_damage": 2,
-		"normal_damage": 3,
-		"critical_damage": 6
-	},
 	"big_fire_ball": {
 		"id": "big_fire_ball",
 		"type": "attack",
@@ -53,7 +25,36 @@ const SKILLS = {
 		"normal_damage": 10,
 		"critical_damage": 20
 	},
+	"fire_cloud": {
+		"id": "fire_cloud",
+		"type": "attack",
+		"name": "火云",
+		"weak_damage": 5,
+		"normal_damage": 10,
+		"critical_damage": 15
+	},
+	"fire_wrath_spash": {
+		"id": "fire_wrath_spash",
+		"type": "attack",
+		"name": "愤怒喷射",
+		"weak_damage": 1,
+		"normal_damage": 5,
+		"critical_damage": 20
+	},
+	"fire_attack": {
+		"id": "fire_attack",
+		"type": "buffer",
+		"name": "炙热能量",
+		"attack": 2,
+	},
+	"big_fire_attack": {
+		"id": "big_fire_attack",
+		"type": "buffer",
+		"name": "灼热力量",
+		"attack": 5
+	},
 	
+	# water
 	"water_ball": {
 		"id": "water_ball",
 		"type": "attack",
@@ -62,13 +63,37 @@ const SKILLS = {
 		"normal_damage": 2,
 		"critical_damage": 5
 	},
+	"big_water_ball": {
+		"id": "big_water_ball",
+		"type": "attack",
+		"name": "水龙弹术",
+		"weak_damage": 2,
+		"normal_damage": 5,
+		"critical_damage": 15
+	},
+	"water_drop": {
+		"id": "water_drop",
+		"type": "attack",
+		"name": "瀑布",
+		"weak_damage": 3,
+		"normal_damage": 6,
+		"critical_damage": 18
+	},
+	"water_flood": {
+		"id": "water_flood",
+		"type": "attack",
+		"name": "大洪灾",
+		"weak_damage": 3,
+		"normal_damage": 7,
+		"critical_damage": 13
+	},
 	"big_rain": {
 		"id": "big_rain",
 		"type": "attack",
 		"name": "暴雨术",
-		"weak_damage": 3,
-		"normal_damage": 5,
-		"critical_damage": 10
+		"weak_damage": 5,
+		"normal_damage": 10,
+		"critical_damage": 20
 	},
 	"heal": {
 		"id": "heal",
@@ -83,21 +108,46 @@ const SKILLS = {
 		"heal": 20
 	},
 	
-	"stone_crash": {
-		"id": "stone_crash",
-		"type": "attack",
-		"name": "落石",
-		"weak_damage": 1,
-		"normal_damage": 2,
-		"critical_damage": 10
-	},
+	# stone
 	"stone_spike": {
-		"id": "stone_crash",
+		"id": "stone_spike",
 		"type": "attack",
 		"name": "土锥术",
 		"weak_damage": 1,
 		"normal_damage": 2,
 		"critical_damage": 5
+	},
+	"big_stone_spike": {
+		"id": "big_stone_spike",
+		"type": "attack",
+		"name": "土锥雨术",
+		"weak_damage": 3,
+		"normal_damage": 5,
+		"critical_damage": 15
+	},
+	"stone_erathquake": {
+		"id": "stone_erathquake",
+		"type": "attack",
+		"name": "地震术",
+		"weak_damage": 5,
+		"normal_damage": 8,
+		"critical_damage": 20
+	},
+	"stone_erath_spash": {
+		"id": "stone_erath_spash",
+		"type": "attack",
+		"name": "地裂术",
+		"weak_damage": 3,
+		"normal_damage": 5,
+		"critical_damage": 15
+	},
+	"stone_crash": {
+		"id": "stone_crash",
+		"type": "attack",
+		"name": "落石",
+		"weak_damage": 5,
+		"normal_damage": 10,
+		"critical_damage": 20
 	},
 	"stone_shield": {
 		"id": "stone_shield",
@@ -144,7 +194,7 @@ const ELEMENTS = {
 	"firm": {
 		"type": "mental",
 		"id": "firm",
-		"name": "猛烈"
+		"name": "冲动"
 	},
 	"warm": {
 		"type": "mental",
@@ -160,28 +210,37 @@ const ELEMENTS = {
 
 const GENERATE_SKILL_TABLE = [
 	["fire", "concentrated", "fire_ball"],
-	["fire", "firm", "fire_missile"],
+	["fire_ball", "concentrated", "big_fire_ball"],
+	["fire_ball", "firm", "fire_cloud"],
+	["fire_ball", "crash", "fire_rain"],
+	["fire_ball", "wrath", "fire_wrath_spash"],
 	["fire", "warm", "fire_attack"],
 	["fire_attack", "warm", "big_fire_attack"],
-	["fire", "wrath", "fire_whip"],
-	["fire_ball", "firm", "big_fire_ball"],
-	["big_fire_ball", "firm", "fire_rain"],
+	["fire", "wrath", "fire_wrath_spash"],
+	["fire", "firm", "fire_cloud"],
+	["fire", "crash", "fire_rain"],
 	
+	["water", "concentrated", "water_ball"],
+	["water_ball", "concentrated", "big_water_ball"],
+	["water_ball", "firm", "water_drop"],
+	["water_ball", "wrath", "water_flood"],
+	["water_ball", "crash", "big_rain"],
 	["water", "warm", "heal"],
 	["heal", "concentrated", "big_heal"],
-	["water", "concentrated", "water_ball"],
+	["water", "wrath", "water_ball"],
 	["water", "firm", "water_ball"],
-	["water", "firm", "big_rain"],
-	["water_ball", "firm", "big_rain"],
+	["water", "crash", "big_rain"],
 	
-	["mud", "concentrated", "stone_pike"],
-	
-	["mud", "crash", "stone_crash"],
+	["mud", "concentrated", "stone_spike"],
+	["stone_spike", "concentrated", "big_stone_spike"],
+	["stone_spike", "firm", "stone_erath_spash"],
+	["stone_spike", "crash", "stone_crash"],
+	["stone_spike", "wrath", "stone_erathquake"],
 	["mud", "warm", "stone_shield"],
 	["stone_shield", "firm", "big_stone_shield"],
-	
-	["mud", "cool", "petrification"],
-	["ice", "cool", "frezz"]
+	["mud", "wrath", "stone_spike"],
+	["mud", "firm", "stone_spike"],
+	["mud", "crash", "stone_crash"],
 ]
 
 const CREATURES = {
@@ -192,8 +251,8 @@ const CREATURES = {
 			"max_hp": 3,
 			"current_hp": 3
 		},
-		"weak": ["fire_ball", "fire_missile", "big_fire_ball", "fire_rain"],
-		"strong": ["water_ball", "big_rain"],
+		"weak": ["fire_ball", "big_fire_ball", "fire_rain", "fire_cloud", "fire_wrath_spash"],
+		"strong": ["water_ball", "big_rain", "big_water_ball", "water_drop", "water_flood"],
 		"attack": [1,1],
 		"portrait": "res://assets/creatures/slime.png"
 	},
@@ -204,8 +263,11 @@ const CREATURES = {
 			"max_hp": 10,
 			"current_hp": 10
 		},
-		"weak": ["stone_crash"],
-		"strong": ["fire_ball"],
+		"weak": ["stone_spike", "big_stone_spike", "stone_erath_spash", "stone_crash", "stone_erathquake"],
+		"strong": [
+			"fire_ball", "big_fire_ball", "fire_rain", "fire_cloud", "fire_wrath_spash",
+			"water_ball", "big_rain", "big_water_ball", "water_drop", "water_flood"
+		],
 		"attack": [2,4],
 		"portrait": "res://assets/creatures/skleton.png"
 	},
@@ -216,7 +278,7 @@ const CREATURES = {
 			"max_hp": 20,
 			"current_hp": 20
 		},
-		"weak": ["fire_ball", "big_fire_ball"],
+		"weak": ["fire_ball", "big_fire_ball", "fire_rain", "fire_cloud", "fire_wrath_spash"],
 		"strong": [],
 		"attack": [3,6],
 		"portrait": "res://assets/creatures/slime.png"
@@ -228,8 +290,8 @@ const CREATURES = {
 			"max_hp": 50,
 			"current_hp": 50
 		},
-		"weak": ["water_ball", "big_rain"],
-		"strong": ["fire_ball", "big_fire_ball"],
+		"weak": ["water_ball", "big_rain", "big_water_ball", "water_drop", "water_flood"],
+		"strong": ["fire_ball", "big_fire_ball", "fire_rain", "fire_cloud", "fire_rain", "fire_wrath_spash"],
 		"attack": [4,7],
 		"portrait": "res://assets/creatures/slime.png"
 	},
